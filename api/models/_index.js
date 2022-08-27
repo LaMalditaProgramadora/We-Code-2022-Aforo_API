16d8e730e387fa6236677cff042b28b00fd9c848
AbelardoTM <abel_27_1997@hotmail.com>
2022-08-27 12:36:58 -0500

    Solucion error sintaxis, falta conexion

--- api/models/_index.js
+++ api/models/_index.js
@@ -1,5 +1,5 @@
 import District from "./district.model.js";
 import Bank from "./bank.model.js";
-import Capacicty from "./capacity.model.js";
+import Capacity from "./capacity.model.js";
 
-export { District, Bank, Capacicty };
+export { District, Bank, Capacity };
